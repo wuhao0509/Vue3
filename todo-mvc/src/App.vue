1a7b764a58b760c6dde74b5530aeab6af41a7440
wuhao92 <wuhao92@meituan.com>
2022-04-21 23:14:32 +0800

    feat: 实现静态页

--- todo-mvc/src/App.vue
+++ todo-mvc/src/App.vue
@@ -1,21 +1,67 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="app">
+    <section class="todoapp">
+      <header class="header">
+        <h1>todos</h1>
+        <input
+          class="new-todo"
+          autofocus=""
+          autocomplete="off"
+          placeholder="What needs to be done?"
+        />
+      </header>
+      <section class="main">
+        <input id="toggle-all" class="toggle-all" type="checkbox" />
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+          <li class="todo">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>学习composition api</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" />
+          </li>
+          <li class="todo">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>投递50封简历</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" />
+          </li>
+          <li class="todo">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>上午10:30 参加面试</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" />
+          </li>
+        </ul>
+      </section>
+      <footer class="footer">
+        <span class="todo-count">
+          <strong>3</strong>
+          <span>items left</span>
+        </span>
+        <ul class="filters">
+          <li><a href="#/all" class="selected">All</a></li>
+          <li><a href="#/active" class="">Active</a></li>
+          <li><a href="#/completed" class="">Completed</a></li>
+        </ul>
+        <button class="clear-completed" style="display: none">
+          Clear completed
+        </button>
+      </footer>
+    </section>
+</div>
+
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+export default {
+
 }
-</style>
+</script>
+
